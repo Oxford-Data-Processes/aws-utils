a0b12d03894f1ac700e8c98d58382927b052086d
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-10-16 14:04:51 +0100

    Error catch sqs

--- aws_utils/sqs.py
+++ aws_utils/sqs.py
@@ -21,25 +21,33 @@ class SQSHandler:
 
     def get_all_sqs_messages(self, queue_url):
         all_messages = []
-        while True:
-            # Receive messages from the SQS queue
-            response = self.sqs_client.receive_message(
-                QueueUrl=queue_url,
-                MaxNumberOfMessages=10,
-                WaitTimeSeconds=5,
-                MessageAttributeNames=["All"],
-            )
+        try:
+            while True:
+                # Receive messages from the SQS queue
+                response = self.sqs_client.receive_message(
+                    QueueUrl=queue_url,
+                    MaxNumberOfMessages=10,
+                    WaitTimeSeconds=5,
+                    MessageAttributeNames=["All"],
+                )
 
-            # Check if there are any messages
-            messages = response.get("Messages", [])
-            if not messages:
-                break  # Exit the loop if no more messages
+                # Check if there are any messages
+                messages = response.get("Messages", [])
+                if not messages:
+                    print("No more messages in the queue.")
+                    break  # Exit the loop if no more messages
 
-            for message in messages:
-                timestamp = self.extract_datetime_from_sns_message(message["Body"])
-                message_body = message["Body"]
-                all_messages.append({"timestamp": timestamp, "message": message_body})
+                for message in messages:
+                    timestamp = self.extract_datetime_from_sns_message(message["Body"])
+                    message_body = message["Body"]
+                    all_messages.append(
+                        {"timestamp": timestamp, "message": message_body}
+                    )
 
-        all_messages.sort(key=lambda x: x["timestamp"])
+            if not all_messages:
+                raise Exception("No messages were retrieved from the queue.")
 
-        return all_messages
+            all_messages.sort(key=lambda x: x["timestamp"])
+            return all_messages
+        except Exception as e:
+            raise Exception(f"An error occurred while retrieving messages: {e}")
