37b626d05900b170e87ea7e5264e8082e75c1a30
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-10-16 09:45:34 +0100

    Find packages

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="aws-utils",  # Replace with your package name
-    version="0.1.0",  # Initial version
+    name="aws-utils",
+    version="0.1.0",
     author="Christopher Little",
     author_email="chris@speedsheet.co.uk",
     description="Package for interacting with AWS",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Oxford-Data-Processes/aws-utils",  # URL to your project
-    packages=find_packages(),  # Automatically find packages in your project
+    url="https://github.com/Oxford-Data-Processes/aws-utils",
+    packages=find_packages(
+        include=["aws_utils", "aws_utils.*"]
+    ),  # Explicitly include the package
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",  # Change to your license
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.11",  # Specify the Python version required
+    python_requires=">=3.11",
     install_requires=[
         "boto3",
     ],
