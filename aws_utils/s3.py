e4937e2707ca3ac69c6a05954c09b28dcb80d8ca
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-10-16 14:00:05 +0100

    Add s3

---
+++ aws_utils/s3.py
@@ -0,0 +1,21 @@
+import boto3
+import csv
+import os
+from io import StringIO
+
+
+class S3Client:
+    def __init__(self):
+        self.s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+            region_name=os.environ["AWS_REGION"],
+        )
+
+    def load_csv_from_s3(self, bucket_name, csv_key):
+        csv_object = self.s3_client.get_object(Bucket=bucket_name, Key=csv_key)
+        csv_data = csv_object["Body"].read().decode("utf-8")
+        csv_reader = csv.reader(StringIO(csv_data))
+        data = [row for row in csv_reader]
+        return data
