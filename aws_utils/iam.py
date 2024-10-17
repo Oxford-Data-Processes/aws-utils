8e5458190fc3253c5f6648d97ef6ebfd3573bbe2
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-10-17 06:04:57 +0100

    update get credentials

--- aws_utils/iam.py
+++ aws_utils/iam.py
@@ -25,35 +25,30 @@ class IAM:
 
 
 class AWSCredentials(IAM):
-    def __init__(self, aws_access_key_id, aws_secret_access_key, stage="dev"):
-        super().__init__(stage)
-        self.aws_account_id = self.get_aws_account_id()
-        self.aws_region = "eu-west-2"
-        self.role = self.get_iam_role()
-        self.role_arn = f"arn:aws:iam::{self.aws_account_id}:role/{self.role}"
-        self.aws_access_key_id = aws_access_key_id
-        self.aws_secret_access_key = aws_secret_access_key
+    @staticmethod
+    def get_aws_credentials(aws_access_key_id, aws_secret_access_key, iam_instance):
 
-    def get_aws_credentials(self):
+        aws_account_id = iam_instance.get_aws_account_id()
+        role = iam_instance.get_iam_role()
+        aws_region = "eu-west-2"
+        role_arn = f"arn:aws:iam::{aws_account_id}:role/{role}"
+
         session_name = f"MySession-{int(time.time())}"
 
         sts_client = boto3.client(
             "sts",
-            aws_access_key_id=self.aws_access_key_id,
-            aws_secret_access_key=self.aws_secret_access_key,
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
         )
 
         response = sts_client.assume_role(
-            RoleArn=self.role_arn, RoleSessionName=session_name
+            RoleArn=role_arn,
+            RoleSessionName=session_name,
         )
 
         credentials = response["Credentials"]
-        aws_access_key_id = credentials["AccessKeyId"]
-        aws_secret_access_key = credentials["SecretAccessKey"]
-        aws_session_token = credentials["SessionToken"]
-
-        os.environ["AWS_ACCESS_KEY_ID"] = aws_access_key_id
-        os.environ["AWS_SECRET_ACCESS_KEY"] = aws_secret_access_key
-        os.environ["AWS_SESSION_TOKEN"] = aws_session_token
-        os.environ["AWS_REGION"] = self.aws_region
-        os.environ["AWS_ACCOUNT_ID"] = self.aws_account_id
+        os.environ["AWS_ACCESS_KEY_ID"] = credentials["AccessKeyId"]
+        os.environ["AWS_SECRET_ACCESS_KEY"] = credentials["SecretAccessKey"]
+        os.environ["AWS_SESSION_TOKEN"] = credentials["SessionToken"]
+        os.environ["AWS_REGION"] = aws_region
+        os.environ["AWS_ACCOUNT_ID"] = aws_account_id
--- test_aws.py
+++ test_aws.py
@@ -1,23 +1,25 @@
 from aws_utils import iam, logs
 import streamlit as st
+import os
 
 # Test parameters
 bucket_name = "rtg-automotive-bucket-654654324108"
 project_name = "frontend"
 
 # Initialize AWS credentials
-aws_access_key_id = st.secrets["aws_credentials"]["AWS_ACCESS_KEY_ID"]
-aws_secret_access_key = st.secrets["aws_credentials"]["AWS_SECRET_ACCESS_KEY"]
-aws_account_id = st.secrets["aws_credentials"]["AWS_ACCOUNT_ID"]
+os.environ["AWS_ACCESS_KEY_ID_ADMIN"] = st.secrets["aws_credentials"][
+    "AWS_ACCESS_KEY_ID"
+]
+os.environ["AWS_SECRET_ACCESS_KEY_ADMIN"] = st.secrets["aws_credentials"][
+    "AWS_SECRET_ACCESS_KEY"
+]
 
-aws_credentials = iam.AWSCredentials(
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key,
-    stage="dev",
+iam_instance = iam.IAM(stage=os.environ["STAGE"])
+iam.AWSCredentials.get_aws_credentials(
+    aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID_ADMIN"],
+    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY_ADMIN"],
+    iam_instance=iam_instance,
 )
-
-aws_credentials.get_aws_credentials()
-
 # Test for get_logs function
 logs_handler = logs.LogsHandler()
 logs = logs_handler.get_logs(bucket_name, project_name)
